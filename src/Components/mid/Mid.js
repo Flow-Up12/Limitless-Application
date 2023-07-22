da8512964ecdf45379440f970059a5a73d20a7bb
Stevan <stevanadrien@gmail.com>
2023-07-22 21:01:18 +0700

    Responsive Middle Update

--- src/Components/mid/Mid.js
+++ src/Components/mid/Mid.js
@@ -8,17 +8,17 @@ function Mid() {
       <p class="key-text">Our key features</p>
       <p class="slogan-text">With limitless, the e-learning is made easy and more interactive, with all these amazing features:</p>
       <div class="card-container">
-        <div class="card1">
+        <div class="card1" id="all-card">
           <img src={ICON} alt="shipping" class="card-icon" />
           <h3 class="card-title">Puzzles</h3>
           <h4 class="card-description">With LIMITLESS, you can solve some puzzles related to your field of study.</h4>
         </div>
-        <div class="card2">
+        <div class="card2" id="all-card">
           <img src={ICON} alt="shipping" class="card-icon" />
           <h3 class="card-title">Practice Problems</h3>
           <h4 class="card-description">In our website, we provide a lot of amazing and interactive practice problems.</h4>
         </div>
-        <div class="card3">
+        <div class="card3" id="all-card">
           <img src={ICON} alt="shipping" class="card-icon" />
           <h3 class="card-title">Secure Login System</h3>
           <h4 class="card-description">You can authenticate securely and privately, due to Django’s amazing authentication system</h4>
